623151a744c7302bfe8517707c423cde580a75bc
Sowndarya Chandrashekar <105284@tfcorp.com>
2019-02-05 14:44:54 +0530

    changed path to user dir and added test folder

---
+++ src/test/java/Test_search_selenium.java
@@ -0,0 +1,46 @@
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+//import org.apache.log4j.Logger;
+import InvokeDriver.Driver;
+import pages.Goolesearch;
+
+
+
+public class Test_search_selenium extends Driver{
+	
+	//
+	
+	private WebDriver driver;
+	Goolesearch google = null;
+	
+	//public static Logger log = Logger.getLogger(Test_search_selenium.class);
+	
+	@BeforeClass
+	public void setup()
+	{
+		driver= getdriver();
+		
+	}
+	
+	@Test
+	public void firstPageSelenium()
+	{
+		google = new Goolesearch(driver);
+		google.setSearchData("selenium");
+		google.clickSearch();
+		
+	}
+	
+	@Test
+	public void validatePageSelenium() throws InterruptedException {
+		//log.info("validating the search results");
+		google.validateSearch("Selenium - Web Browser Automation");
+		google.clickSelenium();
+		
+	}
+
+}
